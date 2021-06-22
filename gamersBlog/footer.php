6fdaeb34b73871d299cdf861232379ba40015dd7
Renato Chaves <51747413+Renato-Chaves@users.noreply.github.com>
2021-06-21 22:25:53 -0300

    Delete gamersBlog directory

--- gamersBlog/footer.php
+++
@@ -1,8 +0,0 @@
-		<footer>
-		<div id="footer">&copy; <?php date('Y'); ?> <?php bloginfo('name'); ?> Theme was made by Renato Chaves.</div>
-		</footer>
-		</div>
-	</div>
-<?php wp_footer(); ?>
-</body>
-</html>
