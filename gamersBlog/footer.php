6c199c65bc83cbda64ae8f87dc8ba8df5500f369
Renato Chaves <51747413+Renato-Chaves@users.noreply.github.com>
2021-06-21 22:25:00 -0300

    Add files via upload

---
+++ gamersBlog/footer.php
@@ -0,0 +1,8 @@
+		<footer>
+		<div id="footer">&copy; <?php date('Y'); ?> <?php bloginfo('name'); ?> Theme was made by Renato Chaves.</div>
+		</footer>
+		</div>
+	</div>
+<?php wp_footer(); ?>
+</body>
+</html>
